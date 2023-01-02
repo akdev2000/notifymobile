3e10f436ffaa21dcde47accf29473ab93b00fa08
Aanandakrishnan <aanandakrishnan.d@gogosoon.com>
2023-01-02 23:38:44 +0530

    Added mobile notificaition lottie image

--- app/screens/Dashboard.tsx
+++ app/screens/Dashboard.tsx
@@ -1,4 +1,10 @@
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  VirtualizedList,
+} from 'react-native';
 import React, {useEffect} from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
@@ -8,9 +14,15 @@ import {
   logout,
 } from '../utils/helpers';
 import {gql} from 'apollo-boost';
-import {useLazyQuery, useMutation, useQuery} from '@apollo/client';
+import {
+  useLazyQuery,
+  useMutation,
+  useQuery,
+  useSubscription,
+} from '@apollo/client';
 import {getDeviceId, usePowerState} from 'react-native-device-info';
-import {Card} from 'react-native-elements';
+import {Card, Image, ThemeProvider} from 'react-native-elements';
+import FontAweSomeIcon from 'react-native-vector-icons/FontAwesome';
 
 const LOGOUT = gql`
   mutation logoutAll($input: NotificationInputType!) {
@@ -26,7 +38,19 @@ const GET_ALL_AVAILABLE_SESSION = gql`
       UserSession {
         id
         session_id
+        browser_name
+        os
+        createdAt
       }
+    }
+  }
+`;
+
+const SESSION_LOG_OUT_LISTENER = gql`
+  subscription logoutListener {
+    logoutListener {
+      device_id
+      session_id
     }
   }
 `;
@@ -35,7 +59,11 @@ export default function Dashboard(props: any) {
   const [logoutDevice, logoutDeviceResponse] = useMutation(LOGOUT);
   const [getAllAvailableSession, getAllAvailableSessionResponse] = useLazyQuery(
     GET_ALL_AVAILABLE_SESSION,
+    {
+      fetchPolicy: 'no-cache',
+    },
   );
+  const sessionLogoutListener = useSubscription(SESSION_LOG_OUT_LISTENER);
 
   useEffect(() => {
     if (logoutDeviceResponse?.data) {
@@ -64,6 +92,34 @@ export default function Dashboard(props: any) {
       );
     }
   }, [getAllAvailableSessionResponse?.data]);
+
+  useEffect(() => {
+    if (sessionLogoutListener?.data) {
+      console.log(
+        'sessionLogoutListener?.data : ',
+        sessionLogoutListener?.data,
+      );
+      (async () => {
+        if (sessionLogoutListener?.data?.logoutListener) {
+          const {session_id, device_id} =
+            sessionLogoutListener?.data?.logoutListener;
+          const existingSessions =
+            (await getAsyncDataByKey(LOCAL_SESSION_IDS)) || '[]';
+          const s_id: string[] = JSON.parse(existingSessions);
+          const d_id = await getAsyncDataByKey(LOCAL_DEVICE_ID);
+
+          if (d_id == device_id) {
+            session_id?.map((sid: any) => {
+              if (s_id.includes(sid)) {
+                props.navigation.navigate('Home');
+                logout();
+              }
+            });
+          }
+        }
+      })();
+    }
+  }, [sessionLogoutListener?.data]);
 
   useEffect(() => {
     props.navigation.setOptions({
@@ -100,73 +156,140 @@ export default function Dashboard(props: any) {
     })();
   }, []);
 
+  function findBrowserImage(browsername: string) {
+    console.log('test', browsername);
+    switch (browsername) {
+      case 'chrome':
+        return 'chrome';
+      case 'firefox':
+        return 'firefox';
+      default:
+        return 'chrome';
+    }
+  }
+
   return (
     <View
       style={{flex: 1, justifyContent: 'space-around', alignItems: 'center'}}>
       {!getAllAvailableSessionResponse?.loading && (
-        <View>
+        <View
+          style={{
+            width: '100%',
+            padding: 5,
+            flexGrow: 1,
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}>
           {getAllAvailableSessionResponse?.data?.getAvailableSessions?.UserSession.map(
             (session: any) => {
               return (
-                <Text style={{color:"black"}} > 
-                  {session?.session_id}
-                </Text>
-                // <SafeAreaView>
-                //   <View>
-                //     <Card>
-                //       {/*react-native-elements Card*/}
-                //       <Text style={styles.paragraph}>
-                //           {session?.session_id}
-                //       </Text>
-                //     </Card>
-                //   </View>
-                // </SafeAreaView>
-                // <Card title="Local Modules">
-                //   {/*react-native-elements Card*/}
-                //   <Text style={{color: "black"}}>
-                //     {session.session_id}
-                //   </Text>
-                // </Card>
+                <View
+                  // title={session?.os}
+                  style={{
+                    backgroundColor: '#ffffff',
+                    flexGrow: 1,
+                    flexDirection: 'row',
+                    width: '100%',
+                    margin: 10,
+                    padding: 10,
+                    maxHeight: 80,
+                    alignItems: 'center',
+                    justifyContent: 'space-around',
+                  }}
+                  key={session?.session_id}>
+                  <View
+                    style={{
+                      backgroundColor: '#ffffff',
+                      flexGrow: 1,
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                    }}>
+                    <FontAweSomeIcon
+                      name={findBrowserImage(
+                        session?.browser_name?.toLowerCase(),
+                      )}
+                      color="black"
+                      size={30}
+                    />
+                    <View style={{marginLeft: 10}}>
+                      <Text style={{fontWeight: 'bold', color: 'black'}}>
+                        {session?.os}
+                      </Text>
+                      <Text style={styles.paragraph}>
+                        Active From :{' '}
+                        {new Date(session?.createdAt)?.toLocaleDateString()}{' '}
+                        {new Date(session?.createdAt)?.toLocaleTimeString()}
+                      </Text>
+                    </View>
+                  </View>
+                  <View>
+                    <Icon.Button
+                      name="logout"
+                      color={'black'}
+                      style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                      }}
+                      iconStyle={{fontSize: 20, color: '#ffffff'}}
+                      onPress={async () => {
+                        await logoutDevice({
+                          variables: {
+                            input: {
+                              device_id: await getAsyncDataByKey(
+                                LOCAL_DEVICE_ID,
+                              ),
+                              session_id: [session?.session_id],
+                            },
+                          },
+                        });
+                      }}
+                      backgroundColor="#014EDE">
+                      <Text style={{color: '#ffffff'}}>Logout</Text>
+                    </Icon.Button>
+                  </View>
+                </View>
               );
             },
           )}
         </View>
       )}
-      <Icon.Button
-        name="logout"
-        color={'black'}
-        style={{
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-        iconStyle={{fontSize: 30, color: '#ffffff'}}
-        onPress={async () => {
-          let sessionIdArray: string[] = JSON.parse(
-            (await getAsyncDataByKey(LOCAL_SESSION_IDS)) || '[]',
-          );
-          console.log('Data : ', {
-            device_id: await getAsyncDataByKey(LOCAL_DEVICE_ID),
-            session_id: sessionIdArray,
-          });
-          await logoutDevice({
-            variables: {
-              input: {
-                device_id: await getAsyncDataByKey(LOCAL_DEVICE_ID),
-                session_id: sessionIdArray,
+      <View style={{marginBottom: 20}}>
+        <Icon.Button
+          name="logout"
+          color={'black'}
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          iconStyle={{fontSize: 30, color: '#ffffff'}}
+          onPress={async () => {
+            let sessionIdArray: string[] = JSON.parse(
+              (await getAsyncDataByKey(LOCAL_SESSION_IDS)) || '[]',
+            );
+            console.log('Data : ', {
+              device_id: await getAsyncDataByKey(LOCAL_DEVICE_ID),
+              session_id: sessionIdArray,
+            });
+            await logoutDevice({
+              variables: {
+                input: {
+                  device_id: await getAsyncDataByKey(LOCAL_DEVICE_ID),
+                  session_id: sessionIdArray,
+                },
               },
-            },
-          });
-        }}
-        backgroundColor="#014EDE">
-        <Text style={{color: '#ffffff'}}>Logout From All Device</Text>
-      </Icon.Button>
+            });
+          }}
+          backgroundColor="#014EDE">
+          <Text style={{color: '#ffffff'}}>Logout From All Device</Text>
+        </Icon.Button>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   paragraph: {
-    fontWeight: 'bold',
     color: '#34495e',
+    fontSize: 15,
   },
 });
--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,38 +1,99 @@
 import {View, Text} from 'react-native';
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { getAsyncDataByKey } from '../utils/helpers';
+import {getAsyncDataByKey} from '../utils/helpers';
+import Lottie from 'lottie-react-native';
+import lottie from '../utils/mobile_notification.json';
+import {Button} from 'react-native-elements';
 
 export default function Home(props: any) {
   useEffect(() => {
-  (async () => {
-    const sessionIds  = await getAsyncDataByKey("session_ids");
-    console.log("sessionIds : " , sessionIds)
-    if(sessionIds) {
-      let sessionIdArray : string[]= JSON.parse(sessionIds); 
-      if(sessionIdArray.length > 0){
-        props.navigation.navigate("Dashboard")
+    (async () => {
+      const sessionIds = await getAsyncDataByKey('session_ids');
+      console.log('sessionIds : ', sessionIds);
+      if (sessionIds) {
+        let sessionIdArray: string[] = JSON.parse(sessionIds);
+        if (sessionIdArray.length > 0) {
+          props.navigation.navigate('Dashboard');
+        }
       }
-    }
-    
-  })()
-  }, [])
+    })();
+  }, []);
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Connet Now !!</Text>
-      <Icon.Button
-        name="qr-code-scanner"
-        color={'black'}
-        iconStyle={{fontSize: 90}}
+    <View
+      style={{
+        flex: 1,
+        // justifyContent: 'space-around',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}>
+      <View>
+        <Text
+          style={{
+            fontSize: 30,
+            color: 'black',
+            fontFamily: 'sans-serif-medium',
+          }}>
+          Notify Me
+        </Text>
+      </View>
+      <View
         style={{
+          flex: 1,
+          flexDirection: 'column',
           alignItems: 'center',
-          justifyContent: 'center',
-        }}
-        onPress={() => {
-          console.log("Pressed")
-          props.navigation.navigate('Scan');
-        }}
-        backgroundColor="#ffffff"></Icon.Button>
+          // justifyContent: 'space-around',
+        }}>
+        <View style={{marginBottom: 10, marginTop: 10}}>
+          <Text
+            style={{
+              color: 'black',
+              fontSize: 15,
+              marginTop: 5,
+              marginBottom: 2,
+            }}>
+            1. Goto https://www.notifyme.akdev.com
+          </Text>
+          <Text
+            style={{
+              color: 'black',
+              fontSize: 15,
+              marginTop: 5,
+              marginBottom: 2,
+            }}>
+            2. Click on below Scanner
+          </Text>
+          <Text
+            style={{
+              color: 'black',
+              fontSize: 15,
+              marginTop: 5,
+              marginBottom: 2,
+            }}>
+            3. Scan the QR code on your system.
+          </Text>
+        </View>
+        <View style={{marginTop: 10}}>
+          <Icon.Button
+            name="qr-code-scanner"
+            color={'black'}
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            iconStyle={{fontSize: 30, color: '#ffffff'}}
+            onPress={() => {
+              // console.log("Test")
+              props.navigation.navigate('Scan');
+            }}
+            backgroundColor="#014EDE">
+            <Text style={{color: '#ffffff'}}>Connect Now</Text>
+          </Icon.Button>
+        </View>
+      </View>
+      <View>
+        <Lottie source={lottie} autoPlay loop />
+      </View>
     </View>
   );
 }
